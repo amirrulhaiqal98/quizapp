48197922d71e549608e7ba4b07e4c2b592c2d26c
amirrulhaiqal98 <amirrulhaiqal98@gmail.com>
2023-05-31 01:18:09 +0800

    adding options

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import '../constant.dart';
-import '../models/question_model.dart'; //our question model
-import '../widgets/question_widget.dart'; //question widget
+import '../models/question_model.dart';
+import '../widgets/question_widget.dart';
 import '../widgets/next_button.dart';
-
-//create a homeScreen widget
+import '../widgets/option_card.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -21,35 +20,38 @@ class _HomeScreenState extends State<HomeScreen> {
       options: {'5': false, '30': false, '4': true, '10': false},
     ),
     Question(
-      id: '10',
+      id: '20',
       title: 'What is 10 + 20 ?',
       options: {'50': false, '30': true, '40': false, '10': false},
     )
   ];
-  // //create index loop thought _questions
   int index = 0;
+  bool isPressed = false;
 
-  //create a function to  display the  next question
   void nextQuestion() {
     if (index == _question.length - 1) {
       return;
     } else {
       setState(() {
-        index++; //when the index will change to 1. rebuild the app
+        index++;
+        isPressed = false;
       });
     }
   }
 
+  void changeColor() {
+    setState(() {
+      isPressed = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    //change background color
-
     return Scaffold(
       backgroundColor: background,
       appBar: AppBar(
         title: const Text('Quiz App'),
         backgroundColor: background,
-        shadowColor: Colors.transparent,
       ),
       body: Container(
         width: double.infinity,
@@ -63,16 +65,25 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             const Divider(
               color: Colors.white,
-            )
+            ),
+            const SizedBox(height: 25.0),
+            for (int i = 0; i < _question[index].options.length; i++)
+              OptionCard(
+                option: _question[index].options.keys.toList()[i],
+                color: isPressed
+                    ? _question[index].options.values.toList()[i] == true
+                        ? Colors.green
+                        : Colors.red
+                    : Colors.white,
+                onTap: changeColor,
+              ),
           ],
         ),
       ),
-
-      //use the floating action button
       floatingActionButton: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0),
         child: NextButton(
-          nextQuestion: nextQuestion, //the above function
+          nextQuestion: nextQuestion,
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
